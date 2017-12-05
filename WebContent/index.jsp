8bcb6f27b14d0d34efe2b522dc86ee57d4adb5cf
James Lee <jamesusa19@yahoo.com>
2017-12-05 11:13:35 -0800

    Using run button for the mapping tool instead of automatic running. Uses HttpServlets.

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -8,44 +8,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<% 
-	PubTypeOrganizer pubOrganizer = new PubTypeOrganizer().seperatePubTypesForDirectory("TestAssets");
-    // Create an IpdFile object for each 941 file found.
-    List<IpdModel> ipdFilesList = Lists.newArrayList();
-    for (String fileName : pubOrganizer.getIpdFileNameList()) {
-       ipdFilesList.add(new IpdModel(fileName).setParentDirectoryPath("TestAssets"));
-    }
-
-    // Make an ipdParser
-    IpdParser ipdParser = new IpdParser();
-    // For each idpdFile, send it to the parser and set its partNumbers
-    for (IpdModel ipdFile : ipdFilesList) {
-       ipdParser.setPartNumbersForFile(ipdFile);
-    }
-
-    // Create the WucBaseline and comparer
-    Comparer comparer = new Comparer(new WucBaselineData("TestAssets/MQ-4C_UAV_WUCBaselineReportAsOf2017-06-12.txt"));
-
-    // For each IpdFile, send it to the WucBaseline and have the wucs be set
-    for (IpdModel ipdFile : ipdFilesList) {
-       ipdFile.setWucsThatMatchPartNumbers(comparer.findAllWucEntriesForFile(ipdFile));
-    }
-
-    // Create a WucMappingFileWriter
-    WucMappingFileWriter wucMappingFileWriter = new WucMappingFileWriter("TestAssets/testWucWriterFile.csv");
-    // Send the ipdFiles to the wucMappingFileWriter to be written out to the
-    // file
-    out.print("<h1>Generating Csv File</h1>");
-    for (IpdModel ipdFile : ipdFilesList) {
-       wucMappingFileWriter.printEntriesForIpdFile(ipdFile);
-    }
-    wucMappingFileWriter.close();
-
-    // Log Those files not Listed
-    WucMappingGeneratorLogger logger = new WucMappingGeneratorLogger("TestAssets/wucGeneratorLog.txt");
-    out.println("<h1>Generating Log File<h3>");
-    logger.logFilesNotCoveredInWucMappingFor(pubOrganizer);
-    logger.close();
-	%>
+	<form action="${pageContext.request.contextPath}/WucMappingGenerator" method="post">
+	    <input type="submit" name="run" value="Run Generator" />
+	</form>
 </body>
 </html>
--- src/ngc/util/wuc/WucMappingGenerator.java
+++ src/ngc/util/wuc/WucMappingGenerator.java
@@ -1,19 +1,26 @@
 package ngc.util.wuc;
 
 import java.util.List;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.*;
 
 import com.google.common.collect.Lists;
 
-public class WucMappingGenerator {
+@WebServlet("/WucMappingGenerator")
+public class WucMappingGenerator extends HttpServlet{
+	private static final long serialVersionUID = 1L;
 
-   public static void main(String[] args) {
+	@Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
       // Send the fileLocation to a PubTypeOrganizer to seperate out pubs.
-      PubTypeOrganizer pubOrganizer = new PubTypeOrganizer().seperatePubTypesForDirectory(args[0]);
+      PubTypeOrganizer pubOrganizer = new PubTypeOrganizer().seperatePubTypesForDirectory("TestAssets");
 
       // Create an IpdFile object for each 941 file found.
       List<IpdModel> ipdFilesList = Lists.newArrayList();
       for (String fileName : pubOrganizer.getIpdFileNameList()) {
-         ipdFilesList.add(new IpdModel(fileName).setParentDirectoryPath(args[0]));
+         ipdFilesList.add(new IpdModel(fileName).setParentDirectoryPath("TestAssets"));
       }
 
       // Make an ipdParser
@@ -24,7 +31,7 @@ public class WucMappingGenerator {
       }
 
       // Create the WucBaseline and comparer
-      Comparer comparer = new Comparer(new WucBaselineData(args[1]));
+      Comparer comparer = new Comparer(new WucBaselineData("TestAssets/MQ-4C_UAV_WUCBaselineReportAsOf2017-06-12.txt"));
 
       // For each IpdFile, send it to the WucBaseline and have the wucs be set
       for (IpdModel ipdFile : ipdFilesList) {
@@ -32,7 +39,7 @@ public class WucMappingGenerator {
       }
 
       // Create a WucMappingFileWriter
-      WucMappingFileWriter wucMappingFileWriter = new WucMappingFileWriter(args[2]);
+      WucMappingFileWriter wucMappingFileWriter = new WucMappingFileWriter("TestAssets/testWucWriterFile.csv");
       // Send the ipdFiles to the wucMappingFileWriter to be written out to the
       // file
       for (IpdModel ipdFile : ipdFilesList) {
@@ -41,9 +48,12 @@ public class WucMappingGenerator {
       wucMappingFileWriter.close();
 
       // Log Those files not Listed
-      WucMappingGeneratorLogger logger = new WucMappingGeneratorLogger(args[3]);
+      WucMappingGeneratorLogger logger = new WucMappingGeneratorLogger("TestAssets/wucGeneratorLog.txt");
       logger.logFilesNotCoveredInWucMappingFor(pubOrganizer);
       logger.close();
+      PrintWriter out = res.getWriter();
+      out.println("Finished");
+      res.sendRedirect(req.getContextPath() + "/index.jsp");
    }
 
 }

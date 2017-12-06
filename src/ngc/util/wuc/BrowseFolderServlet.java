5f3a07f94050b1711a2fb100d45cd76ac343241f
Idiris <idiris.sd@gmail.com>
2017-12-06 08:31:38 -0800

    Added user input for Browse Folder functionality

--- src/ngc/util/wuc/BrowseFolderServlet.java
+++ src/ngc/util/wuc/BrowseFolderServlet.java
@@ -1,19 +1,32 @@
 package ngc.util.wuc;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
 @WebServlet("/BrowseFolder")
-public class BrowseFolderServlet extends HttpServlet {
+public class BrowseFolderServlet extends HttpServlet  {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		JFileChooser fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int returnValue = fc.showOpenDialog(null);
+
 		res.setContentType("text/plain");
 	    res.setCharacterEncoding("UTF-8");
-	    res.getWriter().write("TODO: implement folder browsing");
+		
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = fc.getSelectedFile();
+		    res.getWriter().write(selectedFile.getAbsolutePath());
+		}
+
 	}
 }
